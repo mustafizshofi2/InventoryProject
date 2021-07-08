d879d73b046e4413c21ecb0f0cb473f3e08d282f
mustafizshofi2 <tahdiba.mustafiz@gmail.com>
2021-07-09 01:19:34 +0600

    new update

--- register.php
+++ register.php
@@ -16,6 +16,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
         <link href="CSS/style.css" rel="stylesheet" type="text/css">
+        <link href="CSS/register.css" rel="stylesheet" type="text/css">
 
     </head>
 
@@ -23,45 +24,49 @@
 
       <form method="post" action="register.php" enctype="multipart/form-data">
 
-          <h1>Registration Form</h1>
+           <div class="container">
 
-          <div>
-              <label for="name">Your Name</label>
-              <input type="text" id="name"  name="name" placeholder="Enter your name" required>
-          </div>
+                  <h1>Registration Form</h1>
+               <hr>
 
-          <div>
-              <label for="nam">Your UserName</label>
-              <input type="text" id="uname"  name="uname" placeholder="Enter your username" required>
-          </div>
+                  <div>
+                      <label for="name">Your Name<span>*</span></label>
+                      <input type="text" id="name"  name="name" placeholder="Enter your name" required>
+                  </div>
 
-          <div>
-              <label for="email">Your Email</label>
-              <input type="text" id="email"  name="email" placeholder="Enter your Email" >
-          </div>
+                  <div>
+                      <label for="nam">Your UserName<span>*</span></label>
+                      <input type="text" id="uname"  name="uname" placeholder="Enter your username" required>
+                  </div>
 
-          <div>
-              <label for="pass">Password</label>
-              <input type="password" id="pass"  name="pass" placeholder="Enter your Password" required>
-          </div>
+                  <div>
+                      <label for="email">Your Email</label>
+                      <input type="text" id="email"  name="email" placeholder="Enter your Email" >
+                  </div>
 
-          <div>
-              <label for="r_pass">Confarm Password</label>
-              <input type="password" id="r_pass"  name="rpass" placeholder="Confarm Your Password" required>
-          </div>
+                  <div>
+                      <label for="pass">Password<span>*</span></label>
+                      <input type="password" id="pass"  name="pass" placeholder="Enter your Password" required>
+                  </div>
 
-          <div>
-             <p><span>***</span>By creating an account you agree to our Terms and Privacy</p>
-          </div>
+                  <div>
+                      <label for="r_pass">Confarm Password<span>*</span></label>
+                      <input type="password" id="r_pass"  name="rpass" placeholder="Confarm Your Password" required>
+                  </div>
 
-          <div>
-              <input type="submit" placeholder="Submit" class=" btn btn-success">
-          </div>
+                  <div style="text-align: center">
+                     <p><span>***</span>By creating an account you agree to our Terms and Privacy</p>
+                  </div>
 
-          <div>
-              <p>Already have an account <a href="login.php">Sign in</a> </p>
-          </div>
+                  <div style="text-align: center; padding: 20px;">
+                      <input type="submit" placeholder="Submit" class=" btn btn-success">
+                  </div>
 
+                  <div style="text-align: center">
+                      <p>Already have an account <a href="login.php">Sign in</a> </p>
+                  </div>
+
+           </div>
 
       </form>
 
